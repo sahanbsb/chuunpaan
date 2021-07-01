8597579222f9e924d4b9a8eea8ab93da2feacf35
Sahan Bulathsinhala <sahan@2c8.com>
2021-07-01 20:40:43 +0530

    initial app

---
+++ src/test/java/com/chuunpaan/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuunpaan.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
